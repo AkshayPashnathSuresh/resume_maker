8af1f7ca7b36baabedcff8b46081aa8bae1d0fcb
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-03-13 17:39:46 +0900

    Add Education Details Page Backend

--- app/controllers/education_infos_controller.rb
+++ app/controllers/education_infos_controller.rb
@@ -1,2 +1,20 @@
 class EducationInfosController < ApplicationController
+    def new
+    end
+    
+    def create
+        @education = Education.where(user_id: current_user).first_or_initialize
+        if(@education.update(education_params))
+            #Move to Work History Page
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def education_params
+            params.require(:education).permit(:institution_name, :course_name, :studied_from,
+                                            :studied_to, :grade_point, :achivements)
+        end
 end
