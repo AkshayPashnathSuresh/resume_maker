a3ff18d566dacb213f670bb9fb2b9327a1d33170
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-03-20 11:08:02 +0900

    Resolving CircleCi bugs

--- app/controllers/education_infos_controller.rb
+++ app/controllers/education_infos_controller.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 
 class EducationInfosController < ApplicationController
-    def new
+  def new;end
+ 
+  def create
+    @education = Education.where(user_id: current_user).first_or_initialize
+    if (@education.update(education_params))
+      redirect_to new_work_info_path
+    else
+      render 'new'
     end
-    
-    def create
-        @education = Education.where(user_id: current_user).first_or_initialize
-        if(@education.update(education_params))
-            redirect_to new_work_info_path
-        else
-            render 'new'
-        end
-    end
+  end
 
-    private
+  private
 
-    def education_params
-            params.require(:education).permit(:institution_name, :course_name, :studied_from,
+  def education_params
+    params.require(:education).permit(:institution_name, :course_name, :studied_from,
                                             :studied_to, :grade_point, :achivements)
-        end
+  end
 end
