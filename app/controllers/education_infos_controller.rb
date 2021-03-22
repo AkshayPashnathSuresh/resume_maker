be521ff322439b8846feb3e16c56c4156e2e93aa
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-03-22 23:37:15 +0900

    Resolving CircleCi bild issues

--- app/controllers/education_infos_controller.rb
+++ app/controllers/education_infos_controller.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class EducationInfosController < ApplicationController
-    def new; end
+  def new; end
 
-    def create
-        @education = Education.where(user_id: current_user).first_or_initialize
-        if (@education.update(education_params))
-            # Move to Work History Page
-        else
-            render 'new'
-        end
+  def create
+    @education = Education.where(user_id: current_user).first_or_initialize
+    if @education.update(education_params)
+    # Move to Work History Page
+    else
+      render 'new'
     end
+  end
 
-    private
+  private
 
-    def education_params
-            params.require(:education).permit(:institution_name, :course_name, :studied_from,
-                                              :studied_to,       :grade_point, :achivements)
-        end
+  def education_params
+    params.require(:education).permit(:institution_name, :course_name, :studied_from,
+                                      :studied_to,       :grade_point, :achivements)
+  end
 end
