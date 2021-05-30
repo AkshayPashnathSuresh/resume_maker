eac66d6200e9902aa8caf2394b042da2cc410c59
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-05-30 20:43:25 +0900

    Modifying the seed file and docker related files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'user1@sample.com', password: 'password')
-User.create(email: 'user2@sample.com', password: 'password')
+User.create(email: 'user1@sample.com', password: 'password', first_name:'user1_first_name',
+            last_name: 'user1_last_name', technical_skills: 'user1_technical_skills',known_languages: 'user1_known_languages',
+            area_of_interests: 'user1_area_of_interests', personal_profile: 'user1_personal_profile', carreer_objective: 'user1_carreer_objective')
+User.create(email: 'user2@sample.com', password: 'password', first_name:'user2_first_name',
+            last_name: 'user2_last_name', technical_skills: 'user2_technical_skills',known_languages: 'user2_known_languages',
+            area_of_interests: 'user2_area_of_interests', personal_profile: 'user2_personal_profile', carreer_objective: 'user2_carreer_objective')
