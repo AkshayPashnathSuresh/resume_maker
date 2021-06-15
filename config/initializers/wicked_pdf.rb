d9e5c8fa6fae1f96ec94c6ab659cbb0652a5c774
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-06-15 22:16:19 +0900

    Add Resume Layout and download

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,7 +13,7 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
@@ -24,6 +24,4 @@ WickedPdf.config = {
   # 'xvfb-run' command, in order to simulate an X server.
   #
   # use_xvfb: true,
-  layout: 'resume_pdf.html',
-  default_protocol: 'https'
 }
