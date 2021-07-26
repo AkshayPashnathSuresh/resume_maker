f7fbc7f570b7b46fedb00a9845a1d50354400f5e
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-07-26 23:32:00 +0900

    Add Address Details Edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources :personal_infos, only: [:new, :edit]
+  resources :address_infos, only: [:new, :edit]
   resources :work_infos, only: [:new]
   resources :education_infos, only: [:new]
-  resources :address_infos, only: [:new]
   resources :resume_infos, only: [:new]
   resources :resumes, only: [:show]
   post '/personal_infos/new', to: 'personal_infos#create', as: 'create_personal_infos'
   post '/personal_infos/edit', to: 'personal_infos#update', as: 'update_personal_infos'
   post '/address_infos/new', to: 'address_infos#create', as: 'create_address_infos'
+  post '/address_infos/edit', to: 'address_infos#update', as: 'update_address_infos'
   post '/education_infos/new', to: 'education_infos#create', as: 'create_education_infos'
   post '/work_infos/new', to: 'work_infos#create', as: 'create_work_infos'
   post '/resume_infos/new', to: 'resume_infos#create', as: 'resume_work_infos'
