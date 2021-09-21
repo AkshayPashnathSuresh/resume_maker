394d76932952285bac5efe09d7821b471c72fddf
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-09-21 23:51:56 +0900

    Fix circleci issues

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 class ErrorsController < ApplicationController
-    def not_found
-    end
-  
-    def internal_server_error
-    end
+  def not_found; end
+
+  def internal_server_error; end
 end
--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 module Error
-    module ErrorHandler
-      def self.included(clazz)
-        clazz.class_eval do
-          rescue_from ActiveRecord::RecordNotFound do |e|
-            redirect_to not_found_path
-          end
-          rescue_from StandardError do |e|
-            redirect_to internal_server_error_path
-          end
+  module ErrorHandler
+    def self.included(clazz)
+      clazz.class_eval do
+        rescue_from ActiveRecord::RecordNotFound do |_e|
+          redirect_to not_found_path
+        end
+        rescue_from StandardError do |_e|
+          redirect_to internal_server_error_path
         end
       end
     end
+  end
 end
