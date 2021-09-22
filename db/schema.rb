439eeb2148b7166bc8abc71c8b9496b9654095a9
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-09-22 23:05:15 +0900

    Drop Resumes table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_130945) do
+ActiveRecord::Schema.define(version: 2021_09_22_135440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,15 +99,6 @@ ActiveRecord::Schema.define(version: 2021_09_13_130945) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  create_table "resumes", force: :cascade do |t|
-    t.string "resume_name"
-    t.string "layout_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_resumes_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -137,6 +128,5 @@ ActiveRecord::Schema.define(version: 2021_09_13_130945) do
   add_foreign_key "educations", "users"
   add_foreign_key "personals", "users"
   add_foreign_key "projects", "users"
-  add_foreign_key "resumes", "users"
   add_foreign_key "works", "users"
 end
