e6f5898bc9cc5d5db10bee25af85b8d952997647
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-09-26 23:57:52 +0900

    Disable email input

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,7 +8,7 @@ class Address < ApplicationRecord
   validates :remaining_address, presence: true
   validates :country, presence: true
   validates :post_cd, presence: true
-  validates :mail_id, presence: true
+  # validates :mail_id, presence: true
   validates :phone_no, presence: true
   validates :linkedIn_url, presence: true
   validates :twitter_url, presence: true
