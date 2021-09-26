d6eb43d4557775900fe92dc4128a94bc8e0decff
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-09-26 23:08:35 +0900

    Customize model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe User, type: :model do
   end
 
   describe 'Validations' do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
     it 'is not valid without an email' do
       subject.email = nil
       expect(subject).to_not be_valid
@@ -24,8 +28,9 @@ RSpec.describe User, type: :model do
   end
 
   describe 'Associations' do
+    it { should have_one(:personal) }
     it { should have_one(:address) }
-    it { should have_one(:education) }
-    it { should have_one(:work) }
+    it { should have_many(:educations).class_name('Education') }
+    it { should have_many(:works).class_name('Work') }
   end
 end
