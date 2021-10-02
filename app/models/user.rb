7d1a75d45c2d5f192797858248c7087b1c5d0378
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-10-02 11:21:53 +0900

    Add useraccount delete functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_one :personal
-  has_one :address
-  has_many :educations
-  has_many :works
-  has_many :projects
-  has_many :resumes
+  has_one :personal, dependent: :delete
+  has_one :address, dependent: :delete
+  has_many :educations, dependent: :delete_all
+  has_many :works, dependent: :delete_all
+  has_many :projects, dependent: :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
