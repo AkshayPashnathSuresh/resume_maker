54e9687975002fa6742bfc683bfe3df919a5e0ef
AkshayPashnathSuresh <akshayps96@gmail.com>
2021-10-16 22:55:26 +0900

    Add email and strong password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_FORMAT = /\A
+    (?=.{8,})          # Must contain 8 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x.freeze
+
+  validates :password,
+            presence: true,
+            length: { in: Devise.password_length },
+            format: { with: PASSWORD_FORMAT },
+            confirmation: true,
+            on: :create
+
+  validates :password,
+            allow_nil: true,
+            length: { in: Devise.password_length },
+            format: { with: PASSWORD_FORMAT },
+            confirmation: true,
+            on: :update
 end
